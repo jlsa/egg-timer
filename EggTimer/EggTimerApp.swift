800106b36f569ae98ab21644df6f2b7a5c34a707
Joel Hoekstra <jlsahoekstra@gmail.com>
2020-10-01 11:20:51 +0200

    Initial Commit

---
+++ EggTimer/EggTimerApp.swift
@@ -0,0 +1,17 @@
+//
+//  EggTimerApp.swift
+//  EggTimer
+//
+//  Created by Joel Hoekstra on 01/10/2020.
+//
+
+import SwiftUI
+
+@main
+struct EggTimerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
